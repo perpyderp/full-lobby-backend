97fd7a06d58e70ec3c87499e2aa0461b4c87be23
perpyderp <g4m3rall3n@gmail.com>
2023-04-05 21:57:06 -0400

    Added Spring files

---
+++ src/test/java/com/cuisonet/fulllobby/backend/FullLobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuisonet.fulllobby.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullLobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
