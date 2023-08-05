6ce341d357aee6e1c7eed94adfe503fc251ad22c
perpyderp <g4m3rall3n@gmail.com>
2023-08-04 21:36:51 -0400

    Changed build to utilize neon psql database

--- src/main/java/com/perp/fulllobby/config/DataSourceConfig.java
+++ src/main/java/com/perp/fulllobby/config/DataSourceConfig.java
@@ -15,12 +15,8 @@ public class DataSourceConfig {
     public DataSource dataSource() {
         Dotenv dotenv = null;
         dotenv = Dotenv.configure().load();
-
         DriverManagerDataSource source = new DriverManagerDataSource();
-        source.setDriverClassName("org.hibernate.dialect.MySQL8Dialect");
         source.setUrl(dotenv.get("DB_URL"));
-        source.setUsername(dotenv.get("DB_USER"));
-        source.setPassword(dotenv.get("DB_PASS"));
         return source;
     }
 
