8213f07c6b60e1ff047dde2a45ca702c68273e58
perpyderp <g4m3rall3n@gmail.com>
2023-11-14 11:31:23 -0500

    Added RSAKeyProps to configuration

--- src/main/java/com/perp/fulllobby/FullLobbyApplication.java
+++ src/main/java/com/perp/fulllobby/FullLobbyApplication.java
@@ -2,7 +2,11 @@ package com.perp.fulllobby;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.perp.fulllobby.config.RSAKeyProperties;
+
+@EnableConfigurationProperties(RSAKeyProperties.class)
 @SpringBootApplication
 public class FullLobbyApplication {
 
